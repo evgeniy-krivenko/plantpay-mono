d5696aa577eb2baf772d36182a339e5c5bd83512
Evgeniy Krivenko <evgeniy.krivenko.work@gmail.con>
2022-06-13 11:02:07 +0300

    feat: #34 [BE] Implement api for cart page

--- libs/types/src/cart/cart.ts
+++ libs/types/src/cart/cart.ts
@@ -1,4 +1,12 @@
+import { IProductForUsers } from '../product/product-for-users.interface';
+
 export interface InCart {
   cartId: string;
   inCart: string[];
+}
+
+export interface IVendorWithProduct {
+  name: string;
+  email: string;
+  products: IProductForUsers[];
 }
