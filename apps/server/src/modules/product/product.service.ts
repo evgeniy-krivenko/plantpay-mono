7d11eefc23e4bcdf946dbfaa43697cf2372302ae
Evgeniy Krivenko <evgeniy.krivenko.work@gmail.con>
2022-06-15 23:12:00 +0300

    feat: 43/1 created products endpoint with pagination

--- apps/server/src/modules/product/product.service.ts
+++ apps/server/src/modules/product/product.service.ts
@@ -1,24 +1,46 @@
-import { Injectable } from '@nestjs/common';
-import { IProductForUsers } from '@plantpay-mono/types';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { IDataPagination, IPagination, IProductForUsers } from '@plantpay-mono/types';
 import { ProductRepository, ProductWhereInput } from './repository/product.repository';
 import { GetProductsQuery } from './dto/get-products-query';
 import { ProductStatus } from '@prisma/client';
 import { ProductForUsersDto } from './dto/product-for-users.dto';
+import { ProductPaginationDto } from './dto/product-pagination.dto';
 
 @Injectable()
 export class ProductService {
   constructor(private readonly productRepository: ProductRepository) {}
 
-  async getAllForUsers({ limit, offset, category }: GetProductsQuery): Promise<IProductForUsers[]> {
-    const whereInput: ProductWhereInput = { status: ProductStatus.DRAFT };
-    if (category) {
-      whereInput.category = { slug: category };
+  async getAllForUsers({
+    page: pageFormReq,
+    per_page: perPageFromReq,
+    category,
+  }: GetProductsQuery): Promise<IDataPagination<IProductForUsers>> {
+    const whereInput: ProductWhereInput = {
+      status: ProductStatus.DRAFT,
+      category: { slug: category },
+    };
+
+    const page = pageFormReq || 1;
+    let perPage = perPageFromReq || 20;
+
+    if (perPage > 100) {
+      perPage = 100;
     }
-    const productsGenerator = this.productRepository.getAllPublishedProducts(limit, offset, whereInput);
+
+    const skip = (page - 1) * perPage;
+    const productCount = await this.productRepository.getProductCount(whereInput);
+    const totalPages = Math.ceil(productCount / perPage);
+
+    if (page > totalPages) {
+      throw new HttpException('Такой страницы не существует', HttpStatus.BAD_REQUEST);
+    }
+
+    const productsGenerator = this.productRepository.getAllPublishedProducts(perPage, skip, whereInput);
     const productsForUser: IProductForUsers[] = [];
     for await (const product of productsGenerator) {
       productsForUser.push(new ProductForUsersDto({ ...product }));
     }
-    return productsForUser;
+    const pagination: IPagination = { page, perPage, totalPages };
+    return new ProductPaginationDto({ data: productsForUser, pagination });
   }
 }
---
+++ libs/types/src/common/data-pagination.dto.ts
@@ -0,0 +1,15 @@
+import { IPagination } from '@plantpay-mono/types';
+import { Expose } from 'class-transformer';
+
+export class PaginationDto implements IPagination {
+  @Expose()
+  page!: number;
+  @Expose()
+  perPage!: number;
+  @Expose()
+  totalPages!: number;
+
+  constructor(pagination: Partial<PaginationDto>) {
+    Object.assign(this, pagination);
+  }
+}
